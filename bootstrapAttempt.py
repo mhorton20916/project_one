82321abe0eaafaf425c626e4fcf50ebcf3c167c0
Di Gandy <digandy@Dis-MacBook-Air.local>
2018-03-17 12:12:38 -0500

    frontend

---
+++ bootstrapAttempt.py
@@ -0,0 +1,9 @@
+#Creating visial for pandas dataframe. Begin by importing dependencies.
+from flask import Flask
+from flask_bootstrap import Bootstrap
+
+def create_app():
+  app = Flask(__name__)
+  Bootstrap(app)
+
+  return app
